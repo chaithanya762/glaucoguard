352f77cca754aabb7e9835b5fce3782e9c9c51fd
Chaithanya S <84765756+chaithanya762@users.noreply.github.com>
2024-05-09 14:26:08 +0530

    Update project.py

--- project.py
+++ project.py
@@ -235,5 +235,6 @@ st.markdown("""
     <div class="white-bg" style="padding: 20px; text-align: center;">
         <h2>Thank you for using GlaucoGuard!</h2>
         <p>This application was developed by Chaithanya S , Chinmayi H , Elaine M Paily & Hamsini D .</p>
+        <p>Department of CSE, Vidyavardhaka College of Engineering Mysore</p>
     </div>
 """, unsafe_allow_html=True)
