23e41eb0d407ba41e6809071286ec482665060a8
Chaithanya S <84765756+chaithanya762@users.noreply.github.com>
2025-05-31 16:07:50 +0530

    Update project.py

--- project.py
+++ project.py
@@ -131,10 +131,8 @@ st.markdown("""
             to   { opacity: 1; }
         }
     </style>
-   st.markdown("""<p style='font-size: 20px; text-align: center; background-color: orange; color: black;'>This is a simple image classification web application to predict glaucoma through fundus images of the eye. <strong><em>Please upload fundus images only.</em></strong></p>""", unsafe_allow_html=True)
-
-st.markdown("---")
-
+   <h1 class='title blue-bg'>GlaucoGuard: Gaining Clarity in Glaucoma diagnosis through Deep Learning</h1>Add commentMore actions
+""", unsafe_allow_html=True)
 # Paragraph with content about uploading fundus images
 st.markdown("""<p style='font-size: 20px; text-align: center; background-color: orange; color: black;'>This is a simple image classification web application to predict glaucoma through fundus images of the eye. <strong><em>Please upload fundus images only.</em></strong></p>""", unsafe_allow_html=True)
 
